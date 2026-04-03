f0e3ff4a2e2c7c5b05b6d26bbcb2864ec908d76e
p-H-7 <hpinnepu@gmail.com>
2026-04-02 22:37:37 -0400

    local llm

--- app/src/main/java/com/haas/campusguard/LocalLlmEngine.kt
+++ app/src/main/java/com/haas/campusguard/LocalLlmEngine.kt
@@ -31,7 +31,7 @@ class LocalLlmEngine(private val context: Context) {
             val options = LlmInference.LlmInferenceOptions.builder()
                 .setModelPath(modelFile.absolutePath)
                 .setMaxTokens(512)
-                .build()
+                .ok build()
 
             llmInference = LlmInference.createFromOptions(context, options)
             isInitialized = true
